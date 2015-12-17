61235edd4f5bd4941bbd3ca243e56f60d4eada8a
ucm1auto <microprocessor.m1auto@gmail.com>
2015-12-17 19:03:02 +0100

    project template

---
+++ mini_project.asm
@@ -0,0 +1,8 @@
+
+_main:
+
+;mini_project.c,1 :: 		void main() {
+;mini_project.c,3 :: 		}
+L_end_main:
+	GOTO       $+0
+; end of _main
---
+++ mini_project.cp
@@ -0,0 +1,4 @@
+#line 1 "C:/Users/kafi/Desktop/mini_projects/mini_project.c"
+void main() {
+
+}
