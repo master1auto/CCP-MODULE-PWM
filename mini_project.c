1bf5a8574a496b9f64e4d19b9672e9da724eadb2
ROMEYSSA <123missmessaoudi@gmail.com>
2015-12-26 23:11:19 +0100

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,12 +1,11 @@
 void main() {
 
-          //  CCP2  is output
+     // RC1 is output
      TRISC.RC1 = 0x00;
      // configure CCP2
-     CCP2CON = 0x0C;   // PWM mode; T1OSI, M2 active,M3 active
-     CCPR1 =781 ;                // CCP2 compare value is 781 for 10% duty cycle
-
-     // configure Timer 2
-     PR2 = 7811;                  // Timer 2 max value is (1MHz / 4*16*2kHz)-1 = 7811
-     T2CON = 0x7E;   // Timer 2 prescaler 16
+     CCP2CON = 0x0C; // PWM mode ( CCP2M2, CCP2M3 active)
+     CCPR1 =781 ;   // CCP2 compare value is 781 for 10% duty cycle
+     // configure Timer2 
+     PR2 = 7811;    // Timer2  max value is (1MHz / 4*16*2kHz)-1 = 7811
+     T2CON = 0x7E;   // Timer2  prescaler 16;Timer2 ON
   }
