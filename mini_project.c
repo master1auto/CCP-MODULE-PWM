1e75b2c68d410ac9ee28a029e721814cd656b204
master1auto_ouargla <microprocessor.m1auto@gmail.com>
2015-12-30 12:53:41 +0100

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,30 +1,31 @@
 void mypwm_init(){
 // Initialize PWM2 module at 2KHz
   CCP2CON = 0x0C;            // PWM mode(CCP2M2,CCP2M3 active)
+  T2CON = 0x02;  // timer2 prescalar
   PR2 = 62;                 // Timer2 max value is (8MHz / 4*16*2kHz)-1 = 62
 }
- void mypwm_Set_Duty ()
+ void mypwm_Set_Duty (unsigned char duty)// the duty variable must be either 10 or 80
        {
-        //step(1):signal carré in cycle 80% for 2 s
-        //set 50 duty for PWM2 in 80%
-        CCPR2L = 50;            // CCP2 compare value is 50 for 80% duty cycle
-        T2CON = 0x06;         // Timer2 ON
-        delay_ms(2000);       // 2000 ms delay
-        //step(2):signal carré in cycle 10% for 2 s
-        //set 25 duty for PWM2 in 10%
-        CCPR2L =25 ;       // CCP2 compare value is 25 for 10%duty cycle
+         switch (duty) {  
+         case 10: CCPR2L =25; break; // CCP2 compare value is 25 for 10%duty cycle
+         case 80: CCPR2L =50; break;    //set 50 duty for PWM2 in 80%
+         default:CCPR2L=0;
+         }
        }
-
+void mypwm_stat(){
+T2CON=T2CON | 0x04;  // Timer2 ON
+}
 void main(){
          //  CCP2  is output
           TRISC.RC1 = 0;
           mypwm_init();
-          mypwm_Set_Duty();
+          mypwm_stat();
           while (1)
           {   //repeat steps
-              CCPR2L = 50;   // CCP2 compare value is 50 for 80% duty cycle
+              mypwm_Set_Duty(10);
               delay_ms(2000);  // 2000 ms delay
-              CCPR2L=25;    // CCP2 compare value is 25 for 10%duty cycle
+              mypwm_Set_Duty(80);
+              delay_ms(2000);  // 2000 ms delay
            }
     }
 
