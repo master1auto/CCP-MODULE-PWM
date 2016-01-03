a95aaf20a41ec68f5a8ae8bc2cd15db5ef621905
DJAMEL-AUTOMATIQUE <djamelriad30@gmail.com>
2016-01-03 16:31:53 +0100

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,13 +1,13 @@
 void mypwm_init(){
 // Initialize PWM2 module at 2KHz
-  CCP2CON = 0x0C;            // PWM mode(CCP2M2,CCP2M3 active)
+  CCP2CON = 0x1C;            // PWM mode(CCP2M2,CCP2M3 active)
   T2CON = 0x02;              // timer2 prescalar 16
   PR2 = 62;                 // Timer2 max value is (8MHz / 4*16*2kHz)-1 = 62
 }
  void mypwm_Set_Duty (unsigned char duty)// the duty variable must be either 10 or 80
        {
          switch (duty) {  
-         case 10: CCPR2L =25; break;     // CCP2 compare value is 25 for 10%duty cycle ,set 25 duty for PWM2 in 10%
+         case 10: CCPR2L =6; break;     // CCP2 compare value is 25 for 10%duty cycle ,set 25 duty for PWM2 in 10%
          case 80: CCPR2L =201; break;   // CCP2 compare value is 201 for 80% duty cycle, set 201 duty for PWM2 in 80%
          default:CCPR2L=0;
          }
